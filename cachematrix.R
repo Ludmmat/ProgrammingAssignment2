5b24fda439eb08ca459affdb5e6b1c9de247bdd9
Ludmmat <Ludmmat@users.noreply.github.com>
2015-02-22 00:05:06 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,6 @@
 ## functions do
 
 ## Write a short comment describing this function
-
 # makeCacheMatrix creates a list containing a function to
 # 1. set the value of the matrix
 # 2. get the value of the matrix
